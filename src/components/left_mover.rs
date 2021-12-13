5902199c6d7d5d3898154d7633319395816bccdd
Donovan Dicks <donovan.dicks@outlook.com>
2021-12-12 23:32:00 -0500

    Add left mover tag component

---
+++ src/components/left_mover.rs
@@ -0,0 +1,5 @@
+use specs::{Component, DenseVecStorage};
+use specs_derive::Component;
+
+#[derive(Component)]
+pub struct LeftMover {}
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,5 +1,7 @@
+mod left_mover;
 mod position;
 mod renderable;
 
+pub use left_mover::*;
 pub use position::*;
 pub use renderable::*;
