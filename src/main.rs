0bd2c5af4ae5d33d1ed72fa4658c518274552edd
Donovan Dicks <donovan.dicks@outlook.com>
2021-12-12 23:06:57 -0500

    Updated main to have a simple ecs
    Includes some entities and components to draw those entities

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,75 @@
-use rltk::{GameState, Rltk};
+use rltk::{GameState, Rltk, RGB};
+use rltk_app::components::{Position, Renderable};
+use specs::prelude::*;
+use specs::World;
 
-struct State {}
+struct State {
+    ecs: World,
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "Hello Game World");
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
 }
 
-fn main() -> rltk::BError {
+fn build_context() -> rltk::Rltk {
     use rltk::RltkBuilder;
-    let context = RltkBuilder::simple80x50()
+    RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
-        .build()?;
-    let gs = State {};
+        .build()
+        .expect("Failed to build app context")
+}
+
+fn generate_entities(gs: &mut State) {
+    // player
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+
+    // enemies
+    for i in 0..10 {
+        gs.ecs
+            .create_entity()
+            .with(Position { x: i * 7, y: 20 })
+            .with(Renderable {
+                glyph: rltk::to_cp437('☺'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .build();
+    }
+}
+
+fn register_components(gs: &mut State) {
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+}
+
+fn init_ecs() -> State {
+    let mut gs = State { ecs: World::new() };
+    register_components(&mut gs);
+    generate_entities(&mut gs);
+
+    gs
+}
+
+fn main() -> rltk::BError {
+    let context = build_context();
+    let gs = init_ecs();
+
     rltk::main_loop(context, gs)
 }
