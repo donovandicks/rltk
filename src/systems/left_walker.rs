bf46719ccb035057e5965ee86c3d08bf5ffb34de
Donovan Dicks <donovan.dicks@outlook.com>
2021-12-12 23:31:08 -0500

    Defined left walker system

---
+++ src/systems/left_walker.rs
@@ -0,0 +1,18 @@
+use specs::{Join, ReadStorage, System, WriteStorage};
+
+use crate::components::{LeftMover, Position};
+
+pub struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 {
+                pos.x = 79; // reset to right edge
+            }
+        }
+    }
+}
